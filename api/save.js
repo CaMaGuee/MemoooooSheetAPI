870b1445e0c96a07fc5ad66760ef38f5cb81d98b
CaMaGuee <89383380+CaMaGuee@users.noreply.github.com>
2026-01-27 15:50:04 +0900

    Update save.js

--- api/save.js
+++ api/save.js
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
       });
     }
 
-    const appsScriptUrl = "https://script.google.com/macros/s/AKfycbzZ6HKROpxOjooEaPmW7WbCAw6nRTOiXjs0JEgFxlpZsOBfHNvdP6BuM6cqlEH1BQzCnQ/exec";
+    const appsScriptUrl = "https://script.google.com/macros/s/AKfycbzK3qKePMOdT25xIVI7CNVsS7XMOdDxKwGt8D9nJG9x1QGBCR59rZl5Fprj0dNBQcH_Cg/exec";
 
     console.log("[Vercel] Save 요청 전달", {
       userId,
@@ -72,5 +72,6 @@ export default async function handler(req, res) {
     });
   }
 }
+
 
 
